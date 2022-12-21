fcce16c4c5c9bbeec06f3dc15d45dc23b6aad411
miyajiko <kou.sepak@gmail.com>
2022-12-21 13:30:37 +0900

    リストで情報を持てるようにしました。

--- lib/presentation/top/top_page.dart
+++ lib/presentation/top/top_page.dart
@@ -4,52 +4,42 @@ import 'package:flutter_template/constants/route_path.dart';
 import 'package:flutter_template/presentation/Widget/pokemon_widget.dart';
 import 'package:flutter_template/util/pokemon_suggest.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:flutter/services.dart' show rootBundle;
+
 import '../../domain/pokemon.dart';
-import '../../util/logger.dart';
 
 class TopPage extends HookConsumerWidget {
   const TopPage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final state = ref.watch(pokemonSuggestStateProvider);
-    final pokemon = useState<Pokemon>(const Pokemon(
-      name: "",
-      number: "",
-      h: "",
-      a: "",
-      b: "",
-      c: "",
-      d: "",
-      s: "",
-      sum: "",
-    ));
+    final pokemon = useState<List<Pokemon>>([]);
     return Scaffold(
       appBar: AppBar(
         title: const Text(kPageNameTop),
       ),
-      body: Center(
-          child: Column(
-        children: [
-          Autocomplete(
-            optionsBuilder: (textEditingValue) {
-              if (textEditingValue.text == "") {
-                return <String>[];
-              }
-              return ref
-                  .read(pokemonSuggestStateProvider.notifier)
-                  .getSuggestPokemonName(textEditingValue.text);
-            },
-            onSelected: (String pokemonName) {
-              pokemon.value = ref
-                  .read(pokemonSuggestStateProvider.notifier)
-                  .getPokemon(pokemonName);
-            },
-          ),
-          PokemonWidget(pokemon.value)
-        ],
-      )),
+      body: ListView.builder(
+        itemCount: pokemon.value.length + 1,
+        itemBuilder: (BuildContext context, int index) {
+          if (index == 0) {
+            return Autocomplete(
+              optionsBuilder: (textEditingValue) {
+                if (textEditingValue.text == "") {
+                  return <String>[];
+                }
+                return ref
+                    .read(pokemonSuggestStateProvider.notifier)
+                    .getSuggestPokemonName(textEditingValue.text);
+              },
+              onSelected: (String pokemonName) {
+                pokemon.value = [...pokemon.value..add(ref
+                    .read(pokemonSuggestStateProvider.notifier)
+                    .getPokemon(pokemonName))];
+              },
+            );
+          }
+          return PokemonWidget(pokemon.value[index - 1]);
+        },
+      ),
     );
   }
 }
