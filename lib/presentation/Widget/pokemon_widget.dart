f50332fd4ca745642b9030c916e45542db3db142
miyajiko <kou.sepak@gmail.com>
2022-12-21 13:15:53 +0900

    ポケモンWidgetをいい感じにしました。

--- lib/presentation/Widget/pokemon_widget.dart
+++ lib/presentation/Widget/pokemon_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_template/constants/text_style.dart';
 
 import '../../domain/pokemon.dart';
@@ -8,53 +9,81 @@ class PokemonWidget extends StatelessWidget {
   final Pokemon pokemon;
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Wrap(
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(4.0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
-              pokemon.name,
-              style: textStyleLarge,
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Wrap(
+                    children: [
+                      Text(
+                        pokemon.name,
+                        style: textStyleLarge,
+                      ),
+                    ],
+                  ),
+                  Wrap(
+                    children: [
+                      Text("タイプ:"),
+                      SizedBox(
+                        width: 4,
+                      ),
+                      Text("${pokemon.type1} ${pokemon.type2}"),
+                    ],
+                  ),
+                  Wrap(
+                    children: [
+                      Text("とくせい:"),
+                      SizedBox(
+                        width: 4,
+                      ),
+                      Text(
+                          "${pokemon.ability1} ${pokemon.ability2} ${pokemon.hiddenAbility}"),
+                    ],
+                  ),
+                ],
+              ),
             ),
-            SizedBox(
-              width: 10,
+            SizedBox(width: 4,),
+            Container(
+              padding: EdgeInsets.all(4.0),
+              decoration: BoxDecoration(shape: BoxShape.rectangle,borderRadius: BorderRadius.circular(4),  border: Border.all(),),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("H:${pokemon.h}"),
+                          Text("A:${pokemon.a}"),
+                          Text("B:${pokemon.b}"),
+                        ],
+                      ),
+                      SizedBox(width: 4,),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("C:${pokemon.c}"),
+                          Text("D:${pokemon.d}"),
+                          Text("S:${pokemon.s}"),
+                        ],
+                      ),
+                    ],
+                  ),
+                  Text("Total:${pokemon.sum}"),
+                ],
+              ),
             ),
-            Text(
-              "合計種族値:${pokemon.sum}",
-              style: textStyleLarge,
-            )
           ],
         ),
-        Wrap(
-          children: [
-            Text("HP:${pokemon.h}"),
-            Text("こうげき:${pokemon.a}"),
-            Text("ぼうぎょ:${pokemon.b}"),
-            Text("とくこう:${pokemon.c}"),
-            Text("とくぼう:${pokemon.d}"),
-            Text("すばやさ:${pokemon.s}"),
-          ],
-        ),
-        Wrap(
-          children: [
-            Text("タイプ:"),
-            SizedBox(
-              width: 10,
-            ),
-            Text("${pokemon.type1} ${pokemon.type2}"),
-          ],
-        ),
-        Wrap(
-          children: [
-            Text("とくせい:"),
-            SizedBox(
-              width: 10,
-            ),
-            Text(
-                "${pokemon.ability1} ${pokemon.ability2} ${pokemon.hiddenAbility}"),
-          ],
-        ),
-      ],
+      ),
     );
   }
 }
