23fe08b8a5d60a3867408b11812c07b05902a97c
miyajiko <kou.sepak@gmail.com>
2022-12-21 14:54:42 +0900

    Badgeをつけて順番を表示するようにしました。

--- lib/presentation/top/top_page.dart
+++ lib/presentation/top/top_page.dart
@@ -1,3 +1,4 @@
+import 'package:badges/badges.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_template/constants/route_path.dart';
@@ -18,58 +19,65 @@ class TopPage extends HookConsumerWidget {
       appBar: AppBar(
         title: const Text(kPageNameTop),
       ),
-      body: Column(
-        children: [
-          Autocomplete(
-            optionsBuilder: (textEditingValue) {
-              if (textEditingValue.text == "") {
-                return <String>[];
-              }
-              return ref
-                  .read(pokemonSuggestStateProvider.notifier)
-                  .getSuggestPokemonName(textEditingValue.text);
-            },
-            onSelected: (String pokemonName) {
-              pokemon.value = [
-                ...pokemon.value
-                  ..add(ref
-                      .read(pokemonSuggestStateProvider.notifier)
-                      .getPokemon(pokemonName))
-              ];
-              textEditingController.clear();
-            },
-            fieldViewBuilder: (BuildContext context,
-                TextEditingController fieldTextEditingController,
-                FocusNode fieldFocusNode,
-                VoidCallback onFieldSubmitted) {
-              textEditingController = fieldTextEditingController;
-              return TextField(
-                controller: textEditingController,
-                focusNode: fieldFocusNode,
-                style: const TextStyle(fontWeight: FontWeight.bold),
-              );
-            },
-          ),
-          ReorderableListView.builder(
-            shrinkWrap: true,
-            itemCount: pokemon.value.length,
-            itemBuilder: (BuildContext context, int index) {
-              return InkWell(
-                  key: ValueKey(pokemon.value[index]),
-                  onDoubleTap: () {
-                    pokemon.value = [...pokemon.value..removeAt(index)];
-                  },
-                  child: PokemonWidget(pokemon.value[index]));
-            },
-            onReorder: (int oldIndex, int newIndex) {
-              if (oldIndex < newIndex) {
-                newIndex -= 1;
-              }
-              final Pokemon item = pokemon.value.removeAt(oldIndex);
-              pokemon.value = [...pokemon.value..insert(newIndex, item)];
-            },
-          ),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Autocomplete(
+              optionsBuilder: (textEditingValue) {
+                if (textEditingValue.text == "") {
+                  return <String>[];
+                }
+                return ref
+                    .read(pokemonSuggestStateProvider.notifier)
+                    .getSuggestPokemonName(textEditingValue.text);
+              },
+              onSelected: (String pokemonName) {
+                pokemon.value = [
+                  ...pokemon.value
+                    ..add(ref
+                        .read(pokemonSuggestStateProvider.notifier)
+                        .getPokemon(pokemonName))
+                ];
+                textEditingController.clear();
+              },
+              fieldViewBuilder: (BuildContext context,
+                  TextEditingController fieldTextEditingController,
+                  FocusNode fieldFocusNode,
+                  VoidCallback onFieldSubmitted) {
+                textEditingController = fieldTextEditingController;
+                return TextField(
+                  controller: textEditingController,
+                  focusNode: fieldFocusNode,
+                  style: const TextStyle(fontWeight: FontWeight.bold),
+                );
+              },
+            ),
+            ReorderableListView.builder(
+              shrinkWrap: true,
+              itemCount: pokemon.value.length,
+              itemBuilder: (BuildContext context, int index) {
+                return InkWell(
+                    key: ValueKey(pokemon.value[index]),
+                    onDoubleTap: () {
+                      pokemon.value = [...pokemon.value..removeAt(index)];
+                    },
+                    child: Badge(
+                      position: BadgePosition.topStart(),
+                        badgeColor: Theme.of(context).primaryColorDark,
+                        badgeContent:Text((index + 1).toString()),
+                        child: PokemonWidget(pokemon.value[index])));
+              },
+              onReorder: (int oldIndex, int newIndex) {
+                if (oldIndex < newIndex) {
+                  newIndex -= 1;
+                }
+                final Pokemon item = pokemon.value.removeAt(oldIndex);
+                pokemon.value = [...pokemon.value..insert(newIndex, item)];
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
