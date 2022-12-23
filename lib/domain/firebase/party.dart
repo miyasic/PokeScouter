692883ebcce5f40ba1d6eb508a4b70c7df507c01
miyajiko <kou.sepak@gmail.com>
2022-12-23 17:53:55 +0900

    PartyクラスにcreatedAtを追加

--- lib/domain/firebase/party.dart
+++ lib/domain/firebase/party.dart
@@ -1,4 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+
+import '../../util/json_converter.dart';
 
 part 'party.freezed.dart';
 part 'party.g.dart';
@@ -11,6 +14,7 @@ class Party with _$Party {
     required List<String> partyNameList,
     required List<String> divisorList,
     required Map<String, String> eachMemo,
+    @TimestampConverter() required Timestamp createdAt,
   }) = _Party;
 
   factory Party.fromJson(Map<String, Object?> json) => _$PartyFromJson(json);
