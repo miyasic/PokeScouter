33f84fadbda6ee5b17a35d1028ab04706606a8e2
miyajiko <kou.sepak@gmail.com>
2022-12-23 17:14:14 +0900

    userクラスを作成

---
+++ lib/domain/firebase/user.dart
@@ -0,0 +1,15 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:flutter/foundation.dart';
+
+part 'user.freezed.dart';
+part 'user.g.dart';
+
+@freezed
+class User with _$User {
+  const factory User({
+    required String id,
+  }) = _User;
+
+  factory User.fromJson(Map<String, Object?> json) =>
+      _$UserFromJson(json);
+}
