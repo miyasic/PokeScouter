6962ad21bf141b1b505ad2a77bc352c6dff7c873
miyaji555 <kou.sepak@gmail.com>
2022-12-25 11:07:43 +0900

    iphoneでビルドシマシタ。

--- lib/repository/firestore/refs.dart
+++ lib/repository/firestore/refs.dart
@@ -1,5 +1,3 @@
-import 'dart:html';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_template/constants/firestore.dart';
 import 'package:flutter_template/domain/firebase/party.dart';
