92111caef0ba57c621114c7702490dde9362222e
miyajiko <kou.sepak@gmail.com>
2022-12-27 18:33:34 +0900

    OSごとの条件分岐を実装

---
+++ lib/providers/admob_id_provider.dart
@@ -0,0 +1,20 @@
+import 'dart:io';
+
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+final admobId = Provider<String>((ref) {
+  late final String adUnitId;
+  bool isProd = false;
+  if (isProd) {
+    throw UnimplementedError();
+  } else {
+    if (Platform.isAndroid) {
+      adUnitId = "ca-app-pub-3940256099942544/6978759866";
+    } else if (Platform.isIOS) {
+      adUnitId = "ca-app-pub-3940256099942544/1712485313";
+    } else {
+      throw "不明のOSです。";
+    }
+  }
+  return adUnitId;
+});
