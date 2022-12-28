6a25548865de94e89adec62c1edcabe4ee9052fb
miyajiko <kou.sepak@gmail.com>
2022-12-28 12:09:45 +0900

    本番環境のコードを削除

--- lib/providers/admob_id_provider.dart
+++ lib/providers/admob_id_provider.dart
@@ -5,9 +5,8 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 final admobId = Provider<String>((ref) {
   late final String adUnitId;
   bool isProd = false;
-  if (isProd) {
-    throw UnimplementedError();
-  } else {
+  // Todo: 本番環境の実装をする。
+  if (!isProd) {
     if (Platform.isAndroid) {
       adUnitId = "ca-app-pub-3940256099942544/5224354917";
     } else if (Platform.isIOS) {
