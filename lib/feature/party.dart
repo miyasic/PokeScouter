da7ea9ca25047156f811ba21b85e62a5636b855c
miyajiko <kou.sepak@gmail.com>
2022-12-29 16:11:25 +0900

    リファクタリング

--- lib/feature/party.dart
+++ lib/feature/party.dart
@@ -3,8 +3,8 @@ import 'package:flutter_template/providers/auth_controller.dart';
 import 'package:flutter_template/repository/firestore/firebase.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-/// Room 一覧を購読する StreamProvider。
-final roomsStreamProvider = StreamProvider.autoDispose((ref) {
+/// Party 一覧を購読する StreamProvider。
+final partyStreamProvider = StreamProvider.autoDispose((ref) {
   final userId = ref.watch(authControllerProvider)?.uid;
   if (userId == null) {
     return Stream.value(<Party>[]);
