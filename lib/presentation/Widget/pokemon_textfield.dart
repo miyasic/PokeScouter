d935fce843959579941616d68a6e58cb66ccd3ef
miyajiko <kou.sepak@gmail.com>
2022-12-29 12:28:37 +0900

    ポケモンテキストフィールドを切り出しました。

---
+++ lib/presentation/Widget/pokemon_textfield.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import '../../util/pokemon_suggest.dart';
+
+class PokemonTextField extends ConsumerWidget {
+  const PokemonTextField({super.key,required this.onSelected});
+  final Function(String title) onSelected;
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Autocomplete(
+      optionsBuilder: (textEditingValue) {
+        if (textEditingValue.text == "") {
+          return <String>[];
+        }
+        return ref
+            .read(pokemonSuggestStateProvider.notifier)
+            .getSuggestPokemonName(textEditingValue.text);
+      },
+      onSelected: onSelected,
+      fieldViewBuilder: (BuildContext context,
+          TextEditingController fieldTextEditingController,
+          FocusNode fieldFocusNode,
+          VoidCallback onFieldSubmitted) {
+        return TextField(
+          controller: fieldTextEditingController,
+          focusNode: fieldFocusNode,
+          style: const TextStyle(fontWeight: FontWeight.bold),
+          decoration: InputDecoration(
+            border: const OutlineInputBorder(),
+            suffixIcon: IconButton(
+              onPressed: fieldTextEditingController.clear,
+              icon: const Icon(Icons.clear),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+}
