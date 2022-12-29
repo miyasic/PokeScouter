afcb041f3e0d8fb11c9f1a857dc11279c9e05cf6
miyajiko <kou.sepak@gmail.com>
2022-12-29 15:45:01 +0900

    なぜかPartyListPageが消えていたので復活

---
+++ lib/presentation/history/party_list_page.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_template/presentation/Widget/party_widget.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import '../../feature/party.dart';
+
+class PartyListPage extends ConsumerWidget {
+  const PartyListPage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      body: ref.watch(roomsStreamProvider).when(
+            data: (parties) {
+              return ListView.builder(
+                  itemCount: parties.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    if (parties[index].partyNameList.isEmpty)
+                      return const SizedBox();
+                    return PartyWidget(parties[index]);
+                  });
+            },
+            error: (e, __) {
+              return const SizedBox(
+                child: Text("error"),
+              );
+            },
+            loading: () => const Center(child: CircularProgressIndicator()),
+          ),
+    );
+  }
+}
