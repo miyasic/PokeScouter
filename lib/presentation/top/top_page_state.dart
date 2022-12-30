bbd2ccc2e5a149b8ae55cf403434594b039df695
miyaji555 <kou.sepak@gmail.com>
2022-12-31 01:36:39 +0900

    PrimeNumberを登録できるようにしました。

--- lib/presentation/top/top_page_state.dart
+++ lib/presentation/top/top_page_state.dart
@@ -3,6 +3,7 @@ import 'package:flutter_template/providers/auth_controller.dart';
 import 'package:flutter_template/repository/firestore/firebase.dart';
 import 'package:flutter_template/scaffold_messenger.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:trotter/trotter.dart';
 
 final pokemonListProvider =
     StateNotifierProvider.family<PokemonListState, List<Pokemon>, String>(
@@ -45,7 +46,29 @@ class PokemonListState extends StateNotifier<List<Pokemon>> {
   }
 
   List<String> _getPokemonDivisorList() {
-    return ["未実装"];
+    final primeNumbers =
+        state.map((pokemon) => BigInt.parse(pokemon.primeNumber)).toList();
+
+    final primeNumbersComplemented = [
+      ...primeNumbers,
+      ...List.filled(6 - primeNumbers.length, BigInt.one)
+    ];
+
+    final List<String> divisorList = [];
+    for (int i = 6; i >= 1; i--) {
+      final combinationIndexes = Combinations(i, List.generate(6, (j) => j));
+      for (final indexes in combinationIndexes()) {
+        final result = indexes
+            .map((index) => primeNumbersComplemented[index])
+            .toList()
+            .fold(
+                BigInt.one, (previousValue, element) => previousValue * element)
+            .toString();
+
+        if (!divisorList.contains(result)) divisorList.add(result);
+      }
+    }
+    return divisorList;
   }
 
   setParty({required String title, required Function showLoginDialog}) async {
