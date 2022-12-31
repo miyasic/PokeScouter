86dce13c718b204a82289b98039c44fb7d8a039f
miyaji555 <kou.sepak@gmail.com>
2022-12-31 15:02:55 +0900

    BattleStartPageを用意しました。

--- lib/constants/route_path.dart
+++ lib/constants/route_path.dart
@@ -5,9 +5,11 @@ const kPageNameHistory = "履歴";
 const kPageNameBattleHistory = "対戦履歴";
 const kPageNamePartyList = "パーティ一覧";
 const kPageNamePartyRegister = "パーティ登録";
+const kPageNameBattleStart = "対戦開始";
 
 const kPagePathTop = "/battle";
 const kPagePathNext = "/next";
 const kPagePathLogin = "/login";
 const kPagePathHistory = "/history";
 const kPagePathPartyRegister = "/party_register";
+const kPagePathBattleStart = "/battle/start";
---
+++ lib/presentation/battle_start/battle_start_page.dart
@@ -0,0 +1,99 @@
+import 'package:badges/badges.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_template/constants/route_path.dart';
+import 'package:flutter_template/presentation/Widget/pokemon_textfield.dart';
+import 'package:flutter_template/presentation/Widget/pokemon_widget.dart';
+import 'package:flutter_template/presentation/top/top_page_state.dart';
+import 'package:flutter_template/repository/admob_repository.dart';
+import 'package:flutter_template/util/pokemon_suggest.dart';
+import 'package:go_router/go_router.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import '../../domain/pokemon.dart';
+import '../../util/logger.dart';
+import '../Widget/show_dialog.dart';
+
+class BattleStartPage extends HookConsumerWidget {
+  const BattleStartPage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final pokemonListNotifier =
+        ref.read(pokemonListProvider(kPageNameBattleStart).notifier);
+    List<Pokemon> pokemonListState =
+        ref.watch(pokemonListProvider(kPageNameBattleStart));
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(kPageNameBattleStart),
+        actions: [
+          IconButton(
+            onPressed: () {
+              context.push(kPagePathLogin);
+            },
+            icon: const Icon(Icons.login),
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: PokemonTextField(
+              enabled: pokemonListState.length < 6,
+              onSelected: (String pokemonName) {
+                pokemonListNotifier.addPokemon(ref
+                    .read(pokemonSuggestStateProvider.notifier)
+                    .getPokemon(pokemonName));
+              },
+            ),
+          ),
+          Expanded(
+            child: ReorderableListView.builder(
+              itemCount: pokemonListState.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Padding(
+                  key: ValueKey(pokemonListState[index]),
+                  padding: const EdgeInsets.fromLTRB(16, 8, 16, 0),
+                  child: InkWell(
+                      onDoubleTap: () {
+                        pokemonListNotifier.removePokemon(index);
+                      },
+                      child: Badge(
+                          position: BadgePosition.topStart(),
+                          badgeColor: Theme.of(context).primaryColorDark,
+                          badgeContent: Text((index + 1).toString()),
+                          child: PokemonWidget(pokemonListState[index]))),
+                );
+              },
+              onReorder: (int oldIndex, int newIndex) {
+                pokemonListNotifier.reorderList(oldIndex, newIndex);
+              },
+            ),
+          ),
+          ElevatedButton(
+            onPressed: pokemonListState.isEmpty
+                ? null
+                : () async {
+                    ref
+                        .read(
+                            pokemonListProvider(kPageNameBattleStart).notifier)
+                        .setParty(
+                            title: "パーティの名前",
+                            showLoginDialog: () async {
+                              await showConfirmDialog(
+                                  context: context,
+                                  title: 'ログインしてください。',
+                                  okText: 'ログインページを開く。',
+                                  message: '過去の対戦を表示するにはログインが必要です。',
+                                  function: () {
+                                    context.push(kPagePathLogin);
+                                  });
+                            });
+                  },
+            child: const Text("過去の対戦"),
+          ),
+        ],
+      ),
+    );
+  }
+}
