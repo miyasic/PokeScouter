a3d0275ff1a5f342935a96b3d97417c1f731d41e
miyaji555 <kou.sepak@gmail.com>
2022-12-31 16:16:00 +0900

    対戦情報登録ページで相手のパーティを表示できるようになりました。

--- lib/presentation/battle_memo/battle_memo.dart
+++ lib/presentation/battle_memo/battle_memo.dart
@@ -1,22 +1,46 @@
+import 'package:badges/badges.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_template/constants/route_path.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+import '../../domain/pokemon.dart';
+import '../Widget/pokemon_widget.dart';
+import '../top/top_page_state.dart';
+
 class BattleMemoPage extends ConsumerWidget {
   const BattleMemoPage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final pokemonListNotifier =
+        ref.read(pokemonListProvider(kPageNameBattleStart).notifier);
+    List<Pokemon> pokemonListState =
+        ref.watch(pokemonListProvider(kPageNameBattleStart));
     return Scaffold(
       appBar: AppBar(
         title: const Text(kPageNameBattleMemo),
       ),
-      body: ElevatedButton(
-        child: Text("次へ"),
-        onPressed: () {
-          context.push(kPagePathBattleMemo);
+      body: ReorderableListView.builder(
+        itemCount: pokemonListState.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Padding(
+            key: ValueKey(pokemonListState[index]),
+            padding: const EdgeInsets.fromLTRB(16, 8, 16, 0),
+            child: InkWell(
+                onDoubleTap: () {
+                  pokemonListNotifier.removePokemon(index);
+                },
+                child: Badge(
+                    position: BadgePosition.topStart(),
+                    badgeColor: Theme.of(context).primaryColorDark,
+                    badgeContent: Text((index + 1).toString()),
+                    child: PokemonWidget(pokemonListState[index]))),
+          );
+        },
+        onReorder: (int oldIndex, int newIndex) {
+          pokemonListNotifier.reorderList(oldIndex, newIndex);
         },
       ),
     );
