514592275b321b7e3989683c19c8fe4d10f4b784
miyaji555 <kou.sepak@gmail.com>
2022-12-31 22:09:53 +0900

    パーティ一覧ページでパーティを選択できるようになりましtあ。

--- lib/presentation/history/party_list_page.dart
+++ lib/presentation/history/party_list_page.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_template/constants/route_path.dart';
 import 'package:flutter_template/presentation/Widget/party_widget.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+import '../../constants/shared_preferences.dart';
 import '../../feature/party.dart';
+import '../../repository/shared_preferences.dart';
 
-class PartyListPage extends ConsumerWidget {
+class PartyListPage extends HookConsumerWidget {
   const PartyListPage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final selectedPartyId =
+        useState(ref.read(sharedPreferencesProvider).get(kSharedPrefsPartyId));
     return Scaffold(
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
@@ -26,7 +31,14 @@ class PartyListPage extends ConsumerWidget {
                     if (parties[index].partyNameList.isEmpty) {
                       return const SizedBox();
                     }
-                    return PartyWidget(parties[index]);
+                    return InkWell(
+                        onTap: () async {
+                          await ref.read(sharedPreferencesProvider).setString(
+                              kSharedPrefsPartyId, parties[index].partyId);
+                          selectedPartyId.value = parties[index].partyId;
+                        },
+                        child: PartyWidget(parties[index],
+                            parties[index].partyId == selectedPartyId.value));
                   });
             },
             error: (e, __) {
