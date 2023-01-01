9082c0bd7e955acc7db1c72d933d423e0f367e7e
miyaji555 <kou.sepak@gmail.com>
2023-01-01 12:42:20 +0900

    対戦提案用のメソッドを実装

--- lib/presentation/battle_suggest/battle_suggest_page.dart
+++ lib/presentation/battle_suggest/battle_suggest_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_template/constants/route_path.dart';
+import 'package:flutter_template/presentation/battle_suggest/battle_suggest_state.dart';
+import 'package:flutter_template/presentation/top/top_page_state.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -8,15 +10,32 @@ class BattleSuggestPage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final pokemonListNotifier =
+        ref.read(pokemonListProvider(kPageNameBattleStart).notifier);
+    final battleListState = ref.watch(battleListProvider);
     return Scaffold(
       appBar: AppBar(
         title: const Text(kPageNameBattleSuggest),
       ),
-      body: ElevatedButton(
-        child: const Text("次へ"),
-        onPressed: () {
-          context.push(kPagePathBattleMemo);
-        },
+      body: Column(
+        children: [
+          ListView.builder(
+              shrinkWrap: true,
+              itemCount: battleListState.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Text(battleListState[index].battleId);
+              }),
+          ElevatedButton(
+            child: const Text("次へ"),
+            onPressed: () {
+              final divisorList = pokemonListNotifier.getPokemonDivisorList();
+              print(divisorList);
+              // ref.read(battleListProvider.notifier).fetchBattle(
+              //     divisorList: , showLoginDialog: () {});
+              // context.push(kPagePathBattleMemo);
+            },
+          ),
+        ],
       ),
     );
   }
