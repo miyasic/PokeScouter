4696603d6c73fcdf9664d707007d7b4618aa180f
miyajiko <kou.sepak@gmail.com>
2023-01-05 14:20:43 +0900

    とりあえず自分のパーティを表示して、選出も表示できました。

--- lib/presentation/battle_memo/battle_memo.dart
+++ lib/presentation/battle_memo/battle_memo.dart
@@ -3,12 +3,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_template/constants/firestore.dart';
 import 'package:flutter_template/constants/route_path.dart';
-import 'package:flutter_template/constants/shared_preferences.dart';
 import 'package:flutter_template/constants/text_style.dart';
 import 'package:flutter_template/feature/party.dart';
-import 'package:flutter_template/presentation/Widget/party_widget.dart';
-import 'package:flutter_template/repository/shared_preferences.dart';
 import 'package:flutter_template/scaffold_messenger.dart';
+import 'package:flutter_template/util/pokemon_suggest.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -26,6 +24,7 @@ class BattleMemoPage extends HookConsumerWidget {
         ref.watch(pokemonListProvider(kPageNameBattleStart));
     final opponentOrder = useState<List<int>>([]);
     final myOrder = useState<List<int>>([]);
+    final myOrderNameList = useState<List<String>>([]);
     final memoController = useTextEditingController();
     final result = useState<BattleResult>(BattleResult.win);
     return Scaffold(
@@ -106,23 +105,89 @@ class BattleMemoPage extends HookConsumerWidget {
                 '自分のパーティ',
                 style: textStyleBold,
               ),
-              ref.watch(partyFutureProvider).when(
-                    data: (party) {
-                      if (party == null) {
-                        return const Text("パーティが選択されていません");
-                      }
-                      return PartyWidget(party, false);
-                      // return PartyWidget(party, false);
-                    },
-                    error: (e, __) {
-                      return const SizedBox(
-                        child: Text("error"),
-                      );
-                    },
-                    loading: () =>
-                        const Center(child: CircularProgressIndicator()),
-                  )
             ])),
+            SliverList(
+              delegate: ref.watch(partyFutureProvider).when(
+                  data: (party) {
+                    if (party == null) {
+                      return SliverChildListDelegate(
+                          [const Text("パーティが選択されていません")]);
+                    }
+                    return SliverChildBuilderDelegate(
+                      (BuildContext context, int index) {
+                        final pokemon = ref
+                            .read(pokemonSuggestStateProvider.notifier)
+                            .getPokemon(party!.partyNameList[index]);
+                        return Padding(
+                          padding: const EdgeInsets.fromLTRB(0, 8, 0, 0),
+                          child: InkWell(
+                              onDoubleTap: () {
+                                if (!myOrder.value.contains(index)) {
+                                  myOrder.value = [...myOrder.value, index];
+                                  myOrderNameList.value = [
+                                    ...myOrderNameList.value,
+                                    party!.partyNameList[index]
+                                  ];
+                                } else {
+                                  ref
+                                      .read(scaffoldMessengerHelperProvider)
+                                      .showSnackBar('既に登録されているポケモンです。',
+                                          isWarningMessage: true);
+                                }
+                              },
+                              child: Badge(
+                                  position: BadgePosition.topStart(),
+                                  badgeColor:
+                                      Theme.of(context).primaryColorDark,
+                                  badgeContent: Text((index + 1).toString()),
+                                  child: PokemonWidget(
+                                    pokemon!,
+                                    initialFolded: true,
+                                  ))),
+                        );
+                      },
+                      childCount: party.partyNameList.length,
+                    );
+                    // return PartyWidget(party, false);
+                  },
+                  error: (e, __) {
+                    return SliverChildListDelegate([
+                      const SizedBox(
+                        child: Text("error"),
+                      )
+                    ]);
+                  },
+                  loading: () => SliverChildListDelegate(
+                      [const Center(child: CircularProgressIndicator())])),
+            ),
+            SliverList(
+                delegate: SliverChildListDelegate([
+              Text(
+                '自分の選出',
+                style: textStyleBold,
+              ),
+            ])),
+            SliverList(
+                delegate: SliverChildBuilderDelegate(
+              (BuildContext context, int index) {
+                return Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 8, 0, 0),
+                  child: InkWell(
+                      onDoubleTap: () {
+                        myOrder.value.removeAt(index);
+                        myOrder.value = [...myOrder.value];
+                        myOrderNameList.value.removeAt(index);
+                        myOrderNameList.value = [...myOrderNameList.value];
+                      },
+                      child: Badge(
+                          position: BadgePosition.topStart(),
+                          badgeColor: Theme.of(context).primaryColorDark,
+                          badgeContent: Text((index + 1).toString()),
+                          child: Text(myOrderNameList.value[index]))),
+                );
+              },
+              childCount: myOrder.value.length,
+            )),
             SliverList(
                 delegate: SliverChildListDelegate([
               Text(
