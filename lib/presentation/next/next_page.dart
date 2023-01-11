4208e345db0d063f6cb6a0aedba9e9dfb766e496
miyajiko <kou.sepak@gmail.com>
2023-01-11 13:15:52 +0900

    next_pageをmy_pageにリファクタリング

--- lib/presentation/next/next_page.dart
+++ lib/presentation/next/next_page.dart
@@ -1,22 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:poke_scouter/constants/route_path.dart';
 import 'package:poke_scouter/constants/text_style.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class NextPage extends ConsumerWidget {
-  const NextPage({super.key});
+class MyPage extends ConsumerWidget {
+  const MyPage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(kPageNameNext),
+        title: const Text(kPageNameMy),
       ),
       body: Center(
           child: Column(
         children: [
           Text(
-            kPageNameNext,
+            kPageNameMy,
             style: textStylePlain,
           ),
           Text("textStyleLarge", style: textStyleLarge),
