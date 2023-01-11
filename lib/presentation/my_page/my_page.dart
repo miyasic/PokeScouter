f06db9e08fce7f9e2178db3a72779a3415583f27
miyajiko <kou.sepak@gmail.com>
2023-01-11 17:12:46 +0900

    マイページにログイン画面への遷移とログアウトボタンを実装しました。

--- lib/presentation/my_page/my_page.dart
+++ lib/presentation/my_page/my_page.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:poke_scouter/constants/route_path.dart';
 import 'package:poke_scouter/presentation/Widget/my_page_widget.dart';
+import 'package:poke_scouter/presentation/login/user_state_provider.dart';
+
+import '../../providers/auth_controller.dart';
 
 class MyPage extends ConsumerWidget {
   const MyPage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final userState = ref.watch(userStateProvider);
     return Scaffold(
         appBar: AppBar(
           title: const Text(kPageNameMy),
@@ -17,11 +22,42 @@ class MyPage extends ConsumerWidget {
             SizedBox(
               height: 8,
             ),
-            MyPageTile(
-                title: 'ログイン',
-                icons: Icons.add,
-                iconColor: Colors.grey,
-                onTap: () {}),
+            userState.when(
+              data: (data) {
+                // ログイン済みの場合
+                if (data != null) {
+                  return MyPageTile(
+                      title: 'ログアウト',
+                      icons: Icons.add,
+                      iconColor: Colors.grey,
+                      onTap: () {
+                        ref.read(authControllerProvider.notifier).signOut();
+                      });
+                }
+                // 未ログインの場合
+                return MyPageTile(
+                    title: 'ログイン・新規会員登録',
+                    icons: Icons.add,
+                    iconColor: Colors.grey,
+                    onTap: () {
+                      context.push(kPagePathLogin);
+                    });
+              },
+              error: (_, __) {
+                return const Scaffold(
+                  body: Center(
+                    child: Text('エラーだよ'),
+                  ),
+                );
+              },
+              loading: () {
+                return const Scaffold(
+                  body: Center(
+                    child: CircularProgressIndicator(),
+                  ),
+                );
+              },
+            ),
             MyPageTile(
                 title: 'ダークモード',
                 icons: Icons.add,
