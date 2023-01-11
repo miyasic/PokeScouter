73bd4061bdae5984e2be212087da2f243e38efa7
miyajiko <kou.sepak@gmail.com>
2023-01-11 13:29:05 +0900

    MyPageにリストを配置しました。

--- lib/presentation/next/next_page.dart
+++ lib/presentation/next/next_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:poke_scouter/constants/route_path.dart';
-import 'package:poke_scouter/constants/text_style.dart';
+import 'package:poke_scouter/presentation/Widget/my_page_widget.dart';
 
 class MyPage extends ConsumerWidget {
   const MyPage({super.key});
@@ -9,23 +9,30 @@ class MyPage extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(kPageNameMy),
-      ),
-      body: Center(
-          child: Column(
-        children: [
-          Text(
-            kPageNameMy,
-            style: textStylePlain,
-          ),
-          Text("textStyleLarge", style: textStyleLarge),
-          Text("textStyleMedium", style: textStyleMedium),
-          Text("textStylePlane", style: textStylePlain),
-          Text("textStyleBold", style: textStyleBold),
-          const Text("textStyleNon")
-        ],
-      )),
-    );
+        appBar: AppBar(
+          title: const Text(kPageNameMy),
+        ),
+        body: Column(
+          children: [
+            SizedBox(
+              height: 8,
+            ),
+            MyPageTile(
+                title: 'ログイン',
+                icons: Icons.add,
+                iconColor: Colors.grey,
+                onTap: () {}),
+            MyPageTile(
+                title: 'ダークモード',
+                icons: Icons.add,
+                iconColor: Colors.grey,
+                onTap: () {}),
+            MyPageTile(
+                title: 'パーティ登録',
+                icons: Icons.add,
+                iconColor: Colors.grey,
+                onTap: () {}),
+          ],
+        ));
   }
 }
