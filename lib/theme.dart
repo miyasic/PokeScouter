955fd46bfc68bd7bf5bc93b77b0b705cea75f559
miyajiko <kou.sepak@gmail.com>
2023-01-11 17:55:15 +0900

    themeModeを提供するProviderを用意

--- lib/theme.dart
+++ lib/theme.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:poke_scouter/constants/provider_name.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:poke_scouter/constants/provider_name.dart';
 
 final themeProvider = Provider.family<ThemeData, Brightness>((ref, brightness) {
   return ThemeData(
@@ -8,3 +8,5 @@ final themeProvider = Provider.family<ThemeData, Brightness>((ref, brightness) {
     primarySwatch: Colors.teal,
   );
 }, name: kProviderNameTheme);
+
+final themeModeProvider = StateProvider<ThemeMode>((ref) => ThemeMode.system);
