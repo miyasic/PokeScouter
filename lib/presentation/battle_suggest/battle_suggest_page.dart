ede0f103ced9a01397ef0b2e681513b472cedcc2
miyajiko <kou.sepak@gmail.com>
2023-01-16 18:54:54 +0900

    対戦履歴が空だった時のUIを実装

--- lib/presentation/battle_suggest/battle_suggest_page.dart
+++ lib/presentation/battle_suggest/battle_suggest_page.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:poke_scouter/constants/route_path.dart';
 import 'package:poke_scouter/feature/battle_suggest.dart';
 import 'package:poke_scouter/presentation/Widget/battle_widget.dart';
 import 'package:poke_scouter/presentation/top/top_page_state.dart';
-import 'package:go_router/go_router.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import '../../constants/text_style.dart';
 
 class BattleSuggestPage extends ConsumerWidget {
   const BattleSuggestPage({super.key});
@@ -21,18 +23,28 @@ class BattleSuggestPage extends ConsumerWidget {
       body: Column(
         children: [
           Expanded(
-            child: ListView.builder(
-                controller:
-                    ref.read(battleSuggestProvider.notifier).scrollController,
-                itemCount: battleSuggestState.battles.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return BattleWidget(
-                    battle: battleSuggestState.battles[index],
-                    pokemonNameList: pokemonListNotifier
-                        .map((pokemon) => pokemon.name)
-                        .toList(),
-                  );
-                }),
+            child: battleSuggestState.battles.isEmpty
+                ? Center(
+                    child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      "似たような対戦相手との対戦履歴がありません。\n対戦履歴を登録することで次に似たパーティと対戦する際に表示されます。",
+                      style: textStyleGreyPlain,
+                    ),
+                  ))
+                : ListView.builder(
+                    controller: ref
+                        .read(battleSuggestProvider.notifier)
+                        .scrollController,
+                    itemCount: battleSuggestState.battles.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return BattleWidget(
+                        battle: battleSuggestState.battles[index],
+                        pokemonNameList: pokemonListNotifier
+                            .map((pokemon) => pokemon.name)
+                            .toList(),
+                      );
+                    }),
           ),
           ElevatedButton(
             child: const Text("対戦登録に進む"),
