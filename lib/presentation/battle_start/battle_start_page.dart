e59de8d95ce4aa8884a5ec9adf62f22e7f15c085
miyaji555 <kou.sepak@gmail.com>
2023-01-21 14:14:28 +0900

    tutorial_coach_markを導入して、チュートリアルぽいものを実装できました。

--- lib/presentation/battle_start/battle_start_page.dart
+++ lib/presentation/battle_start/battle_start_page.dart
@@ -1,5 +1,6 @@
 import 'package:badges/badges.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:poke_scouter/constants/route_path.dart';
 import 'package:poke_scouter/presentation/Widget/pokemon_textfield.dart';
 import 'package:poke_scouter/presentation/Widget/pokemon_widget.dart';
@@ -7,8 +8,12 @@ import 'package:poke_scouter/presentation/top/top_page_state.dart';
 import 'package:poke_scouter/util/pokemon_suggest.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:tutorial_coach_mark/tutorial_coach_mark.dart';
 
 import '../../domain/pokemon.dart';
+
+final GlobalKey key1 = GlobalKey();
+final GlobalKey key2 = GlobalKey();
 
 class BattleStartPage extends HookConsumerWidget {
   const BattleStartPage({super.key});
@@ -19,6 +24,62 @@ class BattleStartPage extends HookConsumerWidget {
         ref.read(pokemonListProvider(kPageNameBattleStart).notifier);
     List<Pokemon> pokemonListState =
         ref.watch(pokemonListProvider(kPageNameBattleStart));
+    List<TargetFocus> targets = [];
+    targets.add(TargetFocus(identify: "Target 1", keyTarget: key1, contents: [
+      TargetContent(
+          align: ContentAlign.bottom,
+          child: Container(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  "Titulo lorem ipsum",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      fontSize: 20.0),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 10.0),
+                  child: Text(
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin pulvinar tortor eget maximus iaculis.",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                )
+              ],
+            ),
+          ))
+    ]));
+    targets.add(TargetFocus(identify: "Target 1", keyTarget: key2, contents: [
+      TargetContent(
+          align: ContentAlign.bottom,
+          child: Container(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  "Titulo lorem ipsum",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      fontSize: 20.0),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 10.0),
+                  child: Text(
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin pulvinar tortor eget maximus iaculis.",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                )
+              ],
+            ),
+          ))
+    ]));
+    useEffect(() {
+      TutorialCoachMark(targets: targets).show(context: context);
+    });
     return Scaffold(
       appBar: AppBar(
         title: const Text(kPageNameBattleStart),
@@ -36,6 +97,7 @@ class BattleStartPage extends HookConsumerWidget {
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: PokemonTextField(
+              key: key2,
               enabled: pokemonListState.length < 6,
               onSelected: (String pokemonName) {
                 pokemonListNotifier.addPokemon(ref
@@ -68,6 +130,7 @@ class BattleStartPage extends HookConsumerWidget {
             ),
           ),
           ElevatedButton(
+            key: key1,
             onPressed: pokemonListState.isEmpty
                 ? null
                 : () {
