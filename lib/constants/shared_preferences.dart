b9a4b8839c98e48797ccc9260aaacb0757512b76
miyaji555 <kou.sepak@gmail.com>
2023-01-21 20:02:21 +0900

    対戦登録ページのチュートリアルを実装

--- lib/constants/shared_preferences.dart
+++ lib/constants/shared_preferences.dart
@@ -1,4 +1,5 @@
 const kSharedPrefsPartyId = "party_id";
 const kSharedPrefsShowFirstTutorial = "show_first_tutorial";
 const kSharedPrefsShowBattleStartTutorial = "show_battle_start_tutorial";
-const kSharedPrefsShowBattleSuggestTutorial = "show_battle_Suggest_tutorial";
+const kSharedPrefsShowBattleSuggestTutorial = "show_battle_suggest_tutorial";
+const kSharedPrefsShowBattleRegisterTutorial = "show_battle_register_tutorial";
