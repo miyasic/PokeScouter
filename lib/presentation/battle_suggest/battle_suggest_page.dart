d2e2f224229e0e9a1d8b240ea9a14a07e3607665
miyaji555 <kou.sepak@gmail.com>
2023-06-11 22:25:09 +0900

    不要になったbattle_suggest_stateとstateNotifierのクラスを削除

--- lib/presentation/battle_suggest/battle_suggest_page.dart
+++ lib/presentation/battle_suggest/battle_suggest_page.dart
@@ -3,7 +3,6 @@ import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:poke_scouter/constants/route_path.dart';
 import 'package:poke_scouter/constants/shared_preferences.dart';
-import 'package:poke_scouter/feature/battle_suggest.dart';
 import 'package:poke_scouter/presentation/Widget/battle_widget.dart';
 import 'package:poke_scouter/presentation/Widget/show_dialog.dart';
 import 'package:poke_scouter/presentation/Widget/tutorial_widget.dart';
@@ -23,7 +22,6 @@ class BattleSuggestPage extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final pokemonListNotifier =
         ref.read(pokemonListProvider(kPageNameBattleStart));
-    final battleSuggestState = ref.watch(battleSuggestProvider);
     final admobState = ref.watch(admobRepositoryProvider);
     final showTutorial = ref.watch(showBattleSuggestTutorialProvider);
     return Stack(
@@ -49,14 +47,22 @@ class BattleSuggestPage extends ConsumerWidget {
               Expanded(
                 child: ref.watch(battleSuggestFutureProvider).when(
                     data: (battleWithSimilarities) {
+                      if (battleWithSimilarities == null) {
+                        return Center(
+                            child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            "似たような対戦相手との対戦履歴がありません。\n対戦履歴を登録することで次に似たパーティと対戦する際に表示されます。",
+                            style: textStyleGreyPlain,
+                          ),
+                        ));
+                      }
                       return ListView.builder(
-                          controller: ref
-                              .read(battleSuggestProvider.notifier)
-                              .scrollController,
-                          itemCount: battleSuggestState.battles.length,
+                          // itemCount: battleSuggestState.battles.length,
+                          itemCount: battleWithSimilarities.length,
                           itemBuilder: (BuildContext context, int index) {
                             return BattleWidget(
-                              battle: battleSuggestState.battles[index].battle,
+                              battle: battleWithSimilarities[index].battle,
                               pokemonNameList: pokemonListNotifier
                                   .map((pokemon) => pokemon.name)
                                   .toList(),
