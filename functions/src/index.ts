4c3441cd45dcf099c3e6f10d60242697457ad8e3
miyaji555 <kou.sepak@gmail.com>
2023-06-17 22:40:30 +0900

    退会処理を実装

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,15 +1,41 @@
 import * as functions from "firebase-functions";
-import { getIsEmulator } from "./utils";
+import * as firebaseAdmin from "firebase-admin";
 
-exports.helloWorld = functions
-  .region("asia-northeast1")
-  .https.onCall((data, context) => {
-    const uid = context.auth?.uid;
-    console.log(uid);
-    console.log(data);
-    const result = {
-      result: getIsEmulator() ? "Hello World Emulator" : "Hello World",
-    };
+firebaseAdmin.initializeApp(functions.config().firebase);
 
-    return result;
-  });
+exports.deleteUserAndData = functions.auth.user().onDelete(async (user) => {
+  const db = firebaseAdmin.firestore();
+
+  // Get the user document
+  const userDoc = db.collection("user").doc(user.uid);
+
+  // Get subcollections 'party' and 'battle'
+  const partyCollection = userDoc.collection("party");
+  const battleCollection = userDoc.collection("battle");
+
+  // Delete subcollections
+  await deleteCollection(db, partyCollection);
+  await deleteCollection(db, battleCollection);
+
+  // Delete the user document
+  await userDoc.delete();
+});
+
+/**
+ * Function to delete all documents in a collection.
+ *
+ * @function
+ * @param {Object} _db - The Firestore database instance.
+ * @param {Object} collection - The collection to delete.
+ * @return {Promise} A promise that resolves when all documents in the collection have been deleted.
+ */
+async function deleteCollection(
+  _db: firebaseAdmin.firestore.Firestore,
+  collection: firebaseAdmin.firestore.CollectionReference<firebaseAdmin.firestore.DocumentData>
+) {
+  const docs = await collection.listDocuments();
+
+  for (const doc of docs) {
+    await doc.delete();
+  }
+}
